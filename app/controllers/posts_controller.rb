62987770e53b730397da0c9c57d22405c8d3621d
Chloe Steffens <caforonda103@yahoo.com>
2020-09-01 04:42:26 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,14 @@ class PostsController < ApplicationController
 
   def index
     @authors = Author.all
-    if !params[:author].blank?
-      @posts = Post.by_author(params[:author])
-    elsif !params[:date].blank?
-      if params[:date] == "Today"
-        @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
-      else
-        @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
-      end
+  if !params[:author].blank?
+    @posts = Post.by_author(params[:author])
+  elsif !params[:date].blank?
+    if params[:date] == "Today"
+      @posts = Post.from_today
+    else
+      @posts = Post.old_news
+    end
   else
     @posts = Post.all
   end
